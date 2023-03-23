42c2604b582f61dfab1a4c38a093baee42666a9a
Sidney Lemon <sid.lemon3@gmail.com>
2023-03-23 13:22:22 -0600

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { queenArianna } from "$lib/disney";
+
+  // Log the following values to the console
+
+  // Example: Queen Arianna's name
+  console.log(queenArianna.name);
+
+  // 1. The ID of Queen Arianna
+
+  console.log(queenArianna._id);
+
+  // 2. An image of Queen Arianna
+
+  console.log(queenArianna.imageUrl);
+
+  // 3. Queen Arianna's first film
+
+  console.log(queenArianna.films[0]);
+
+  // 4. Queen Arianna's last TV show
+
+  console.log(queenArianna.tvShows[1]);
+
+  // 5. Queen Arianna's second video game
+
+  console.log(queenArianna.videoGames[1]);
+</script>
+
+<h1>CPNT 200: Achievement 1</h1>
+<p>Please see console</p>
